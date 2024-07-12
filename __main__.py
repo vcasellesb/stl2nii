2e703f9621e4d37ec1bc14d5b97f2cd8c6a70d68
vcasellesb <vcasellesb@uoc.edu>
2024-07-12 11:08:56 +0200

    Implemented rotation and origin weird behavior as flag. Per default, we don't do it.

--- __main__.py
+++ __main__.py
@@ -28,7 +28,11 @@ def stltovtk(input_stl: str, output_folder: str) -> str:
 
     return outfilename
 
-def vtktonii(input_vtk:str, ref: str, output_folder: str, dtype) -> str:
+def vtktonii(input_vtk:str, 
+             ref: str, 
+             output_folder: str, 
+             dtype, 
+             weird_behavior: bool = False) -> str:
     """
     Vtk to nii conversion
     """
@@ -47,7 +51,7 @@ def vtktonii(input_vtk:str, ref: str, output_folder: str, dtype) -> str:
     refnii_ndarray = sitk.GetArrayFromImage(refnii)
     ref_dims = refnii_ndarray.shape[::-1] # flip to match image (maybe should be changed per use case?)
     ref_spacing = refnii.GetSpacing()
-    ref_origin = (0, 0, 0)
+    ref_origin = (0, 0, 0) if weird_behavior else refnii.GetOrigin()
     ref_direction = refnii.GetDirection()
     
     image = vtk.vtkImageData()
@@ -86,26 +90,28 @@ def vtktonii(input_vtk:str, ref: str, output_folder: str, dtype) -> str:
     writer.SetInputConnection(imgstenc.GetOutputPort())
     writer.Write()
 
-    label = sitk.ReadImage(outfilename)
-    label_array = sitk.GetArrayFromImage(label).astype(dtype)
-    assert np.sum(label_array>0) != 0, "Empty mask. Please report this issue (in case it's not supposed to be empty) \
-        at https://github.com/vcasellesb/stl2nii/issues"
-   
-    # this is hardcoded. I've found that it's necessary in the stl files I've encountered. Please consider if it works in your case
-    label_array = rotate_stl(label_array)
+    if weird_behavior:
+        label = sitk.ReadImage(outfilename)
+        label_array = sitk.GetArrayFromImage(label).astype(dtype)
+        assert np.sum(label_array>0) != 0, "Empty mask. Please report this issue (in case it's not supposed to be empty) \
+            at https://github.com/vcasellesb/stl2nii/issues"
     
-    niipostproc = sitk.GetImageFromArray(label_array)
-    niipostproc.SetDirection(ref_direction)
-    niipostproc.SetOrigin(refnii.GetOrigin())
-    niipostproc.SetSpacing(refnii.GetSpacing())
-    sitk.WriteImage(niipostproc, outfilename)
+        # this is hardcoded. I've found that it's necessary in the stl files I've encountered. Please consider if it works in your case
+        label_array = rotate_stl(label_array)
+        
+        niipostproc = sitk.GetImageFromArray(label_array)
+        niipostproc.SetDirection(ref_direction)
+        niipostproc.SetOrigin(refnii.GetOrigin())
+        niipostproc.SetSpacing(ref_spacing)
+        sitk.WriteImage(niipostproc, outfilename)
 
     return outfilename
 
 def stltonii(stl_files_list: List[str], 
              nii_ref: str, 
              dtype,
-             output_folder: str = None):
+             output_folder: str=None,
+             weird_behavior: bool=False):
     """
     Main function. Basically iterates through input and performs confersion in two steps:
         stl -> vtk -> nii
@@ -118,7 +124,8 @@ def stltonii(stl_files_list: List[str],
         
         nii_file_final = vtktonii(transformed_to_vtk, ref = nii_ref, 
                                   output_folder=output_folder, 
-                                  dtype=dtype)
+                                  dtype=dtype,
+                                  weird_behavior=weird_behavior)
         
         os.remove(transformed_to_vtk)
     return nii_file_final
@@ -130,21 +137,22 @@ def rotate_stl(data_array: np.ndarray) -> np.ndarray:
     label_array = np.flip(data_array, 1)
     return label_array
 
-def parse_dtype(dtype):
+def parse_dtype(dtype: str):
     """
     Dumb albeit self-explanatory
     """
+    
     match dtype:
         case 'UINT8':
-            return np.uint8
+            dtype_ = np.uint8
         case 'UINT16':
-            return np.uint16
+            dtype_ = np.uint16
         case 'UINT32':
-            return np.uint32
+            dtype_ = np.uint32
         case 'UINT64':
-            return np.uint64
-        case _:
-            raise ValueError('WTF')
+            dtype_ = np.uint64
+        
+    return dtype_
     
 def run_stl2nii_entrypoint():
     import argparse
@@ -159,7 +167,11 @@ def run_stl2nii_entrypoint():
                         help='Folder were output will be written (default: input_dir/nii)')
     parser.add_argument('-dtype', required=False, default=np.uint8,
                         choices=['UINT8', 'UINT16', 'UINT32', 'UINT64'],
-                        help = 'Data type for the resulting NIFTI label (voxel values).')
+                        help = 'Data type for the resulting NIFTI label (voxel values). Choices are: '
+                        "['UINT8', 'UINT16', 'UINT32', 'UINT64']")
+    parser.add_argument('--weird_behavior', required=False, action='store_true',
+                        help='Set this if you want to activate the weird behavior that is discussed in issue #3 '
+                        'This is just to match the "developer"\'s (vicentcaselles) use case')
     args = parser.parse_args()
 
     if isinstance(args.dtype, str):
@@ -168,7 +180,8 @@ def run_stl2nii_entrypoint():
     stltonii(stl_files_list=args.i, 
              nii_ref=args.ref, 
              output_folder=args.o, 
-             dtype=args.dtype)
+             dtype=args.dtype,
+             weird_behavior=args.weird_behavior)
 
 if __name__ == "__main__":   
     run_stl2nii_entrypoint()
