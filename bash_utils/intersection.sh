e6eb99e2c1797c674e5dd86ee135bd78bc7b8588
vcasellesb <vcasellesb@uoc.edu>
2024-10-09 12:16:32 +0200

    intersection.sh allows control of priority when adding masks

--- bash_utils/intersection.sh
+++ bash_utils/intersection.sh
@@ -1,19 +1,52 @@
 #!/bin/bash
 
-mkdir "$3"tmp_sum/
+# $1 should contain the "bigger" object you want to add $2 to.
+# that is, $2 will be preserved while $1's part that intersects 
+# with $2 will be removed from $1 ($2 has priority).
+
+set -o nounset
+
+reverse=false
+
+while getopts :r opt; do
+    case $opt in 
+        r) reverse=true ;;
+        :) echo "Missing argument for option -$OPTARG"; exit 1;;
+       \?) echo "Unknown option -$OPTARG"; exit 1;;
+    esac
+done
+
+shift $(( OPTIND - 1 ))
+
+working_dir=${3:-"."}
+
+mkdir "$working_dir"tmp_sum/
+
+total_accumulated=$1
+new_object=$2
+
+if $reverse; then
+    echo $total_accumulated has priority over $new_object
+else
+    echo $new_object has priority over $total_accumulated
+fi
 
 # binaritzem la mascara amb tot el que portem sumat
-seg_maths ${1} -bin "$3"tmp_sum/TA-bin.nii.gz
+seg_maths ${total_accumulated} -bin "$working_dir"tmp_sum/TA-bin.nii.gz
 # binaritzem la nova dent o objecte a afegir
-seg_maths ${2} -bin "$3"tmp_sum/NO-bin.nii.gz
+seg_maths ${new_object} -bin "$working_dir"tmp_sum/NO-bin.nii.gz
 
-# Calculem la intersecció
-seg_maths "$3"tmp_sum/TA-bin.nii.gz -mul "$3"tmp_sum/NO-bin.nii.gz "$3"tmp_sum/intersection.nii.gz
+# Compute intersection
+seg_maths "$working_dir"tmp_sum/TA-bin.nii.gz -mul "$working_dir"tmp_sum/NO-bin.nii.gz "$working_dir"tmp_sum/intersection.nii.gz
 
-# Fem el flip de la interseccio per poder esborrar els voxels conflictius
-seg_maths "$3"tmp_sum/intersection.nii.gz -sub 1 -abs "$3"tmp_sum/intersection-inv.nii.gz
+# Flip intersection (everywhere where there is no intersection will be 1, where there is intersection will be 0)
+seg_maths "$working_dir"tmp_sum/intersection.nii.gz -sub 1 -abs "$working_dir"tmp_sum/intersection-inv.nii.gz
 
 # Jo prefereixo l’opció girada després hi ha l’opció girada on és més important el que ja hi ha i esborrem els voxels conflictius de l’objecte
-seg_maths ${1} -mul "$3"tmp_sum/intersection-inv.nii.gz -add ${2} ${1}
+if $reverse; then
+    seg_maths ${new_object} -mul "$working_dir"tmp_sum/intersection-inv.nii.gz -add ${total_accumulated} ${total_accumulated}
+else
+    seg_maths ${total_accumulated} -mul "$working_dir"tmp_sum/intersection-inv.nii.gz -add ${new_object} ${total_accumulated}
+fi
 
-rm -r "$3"tmp_sum/
+rm -r "$working_dir"tmp_sum/
