d56c4e5044fbe1b8ce9ff7e3b0c69e5e52b81979
vcasellesb <vcasellesb@uoc.edu>
2024-10-20 22:00:36 +0200

    output specification for intersection.sh

--- bash_utils/intersection.sh
+++ bash_utils/intersection.sh
@@ -18,12 +18,11 @@ done
 
 shift $(( OPTIND - 1 ))
 
-working_dir=${3:-"."}
-
-mkdir "$working_dir"tmp_sum/
-
 total_accumulated=$1
 new_object=$2
+out=${3:-"$total_accumulated"} # if no other file is specified, we overwrite first arg
+working_dir=${4:-"."}
+mkdir "$working_dir"/tmp_sum/
 
 if $reverse; then
     echo $total_accumulated has priority over $new_object
@@ -32,21 +31,21 @@ else
 fi
 
 # binaritzem la mascara amb tot el que portem sumat
-seg_maths ${total_accumulated} -bin "$working_dir"tmp_sum/TA-bin.nii.gz
+seg_maths ${total_accumulated} -bin "$working_dir"/tmp_sum/TA-bin.nii.gz
 # binaritzem la nova dent o objecte a afegir
-seg_maths ${new_object} -bin "$working_dir"tmp_sum/NO-bin.nii.gz
+seg_maths ${new_object} -bin "$working_dir"/tmp_sum/NO-bin.nii.gz
 
 # Compute intersection
-seg_maths "$working_dir"tmp_sum/TA-bin.nii.gz -mul "$working_dir"tmp_sum/NO-bin.nii.gz "$working_dir"tmp_sum/intersection.nii.gz
+seg_maths "$working_dir"/tmp_sum/TA-bin.nii.gz -mul "$working_dir"/tmp_sum/NO-bin.nii.gz "$working_dir"/tmp_sum/intersection.nii.gz
 
 # Flip intersection (everywhere where there is no intersection will be 1, where there is intersection will be 0)
-seg_maths "$working_dir"tmp_sum/intersection.nii.gz -sub 1 -abs "$working_dir"tmp_sum/intersection-inv.nii.gz
+seg_maths "$working_dir"/tmp_sum/intersection.nii.gz -sub 1 -abs "$working_dir"/tmp_sum/intersection-inv.nii.gz
 
 # Jo prefereixo l’opció girada després hi ha l’opció girada on és més important el que ja hi ha i esborrem els voxels conflictius de l’objecte
 if $reverse; then
-    seg_maths ${new_object} -mul "$working_dir"tmp_sum/intersection-inv.nii.gz -add ${total_accumulated} ${total_accumulated}
+    seg_maths ${new_object} -mul "$working_dir"/tmp_sum/intersection-inv.nii.gz -add ${total_accumulated} ${out}
 else
-    seg_maths ${total_accumulated} -mul "$working_dir"tmp_sum/intersection-inv.nii.gz -add ${new_object} ${total_accumulated}
+    seg_maths ${total_accumulated} -mul "$working_dir"/tmp_sum/intersection-inv.nii.gz -add ${new_object} ${out}
 fi
 
-rm -r "$working_dir"tmp_sum/
+rm -r "$working_dir"/tmp_sum/
