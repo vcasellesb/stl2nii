fa475285ca989dcfdd916c688139084ad8101c3f
Vicent Caselles <vcasellesb@uoc.edu>
2025-09-19 16:21:52 +0200

    refactor sum_all_niftis

--- bash_utils/sum_all_niftis.sh
+++ bash_utils/sum_all_niftis.sh
@@ -1,89 +1,95 @@
 #!/bin/bash
 
+set -e
+
 ## Script to add all parts
 if [[ $# -eq 0 ]]; then
 	echo "No directory specified, using current directory"
-	target_dir="./"
+	target_dir="."
 else
-	target_dir="$1/"
+	target_dir="$1"
 fi
 
-nifti_in_target_files=$(ls "${target_dir}"*.nii.gz | wc -l)
+nifti_in_target_files=$(ls "${target_dir}"/*.nii.gz | wc -l)
 
 if [[ $nifti_in_target_files -lt 1 ]]; then
-	echo "No nifti files found in $target_dir directory, exitting. Please provide a valid dir"
+	echo "No nifti files found in "$target_dir" directory, exitting. Please provide a valid dir"
 	exit 1
 fi
 
 # This part removes the nasty disgusting spaces
 ## TODO: do the same but for the dirs (I'm tired of doing this manually)
-if [[ $(ls "$target_dir"*\ * 2>/dev/null | wc -l) -gt 0 ]]; then
-	for file in "${target_dir}"*' '*; do mv "$file" $(echo $file | tr ' ' '_'); done
+if [[ $(ls "$target_dir"/*\ * 2>/dev/null | wc -l) -gt 0 ]]; then
+	for file in "${target_dir}"/*' '*; do
+		mv "$file" $(echo "$file" | tr ' ' '_')
+	done
 fi
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-if [[ $(ls "$target_dir"[0-9][0-9].nii.gz 2>/dev/null | wc -l) -gt 0 ]]; then
-	patternLR=(-regex ".*/([4][0-9]|6[0-7])\.nii.gz$")
-	patternLL=(-regex ".*/([3][0-9]|6[0-7])\.nii.gz$")
-	patternUR=(-regex ".*/([1][0-9]|6[0-7])\.nii.gz$")
-	patternUL=(-regex ".*/([2][0-9]|6[0-7])\.nii.gz$")
-	mandible=$(ls "$target_dir"[Mm]and[ií]bula.nii.gz 2>/dev/null)
-	nerves=$(ls "$target_dir"[Nn]ervios.nii.gz 2>/dev/null)	
-	maxillary=$(ls "$target_dir"[Mm]axilar.nii.gz 2>/dev/null)
+if [[ $(ls "$target_dir"/*[0-9][0-9]*.nii.gz 2>/dev/null | wc -l) -gt 0 ]]; then
+	patternLR="$target_dir/.*4[1-9].*\.nii.gz$"
+	patternLL="$target_dir/.*3[1-9].*\.nii.gz$"
+	patternUR="$target_dir/.*1[1-9].*\.nii.gz$"
+	patternUL="$target_dir/.*2[1-9].*\.nii.gz$"
+	mandible=$(find "$target_dir" -maxdepth 1 -iregex "$target_dir/mand[ií]u\{0,1\}bl[ea].*\.nii.gz" 2>/dev/null)
+	nerves=$(find "$target_dir" -maxdepth 1 -iregex "$target_dir/nervi\{0,1\}[oe]s.*\.nii.gz" 2>/dev/null)
+	# -E so that we get modern regex. This allows us not to escape {...} and |
+	maxillary=$(find -E "$target_dir" -maxdepth 1 -iregex "$target_dir/.*(maxill{0,1}ary{0,1}|skull).*\.nii.gz" 2>/dev/null)
 else
-	patternLR=(-name "*LR*")
-	patternLL=(-name "*LL*")
-	patternUR=(-name "*UR*")
-	patternUL=(-name "*UL*")
-	mandible=$(ls "$target_dir"*[Mm]and[ií]bula*.nii.gz)
-	nerve1=$(ls "${target_dir}"Nerve* | head -n1)
-	nerve2=$(ls "${target_dir}"Nerve* | head -n2 | tail -1)
-	seg_maths $nerve1 -add $nerve2 "${target_dir}"nerves.nii.gz
-	nerves=$(ls ${target_dir}nerves.nii.gz)
-	maxillary=$(ls "$target_dir"*maxilar.nii.gz)
+	patternLR=".*LR.*$"
+	patternLL=".*LL.*$"
+	patternUR=".*UR.*$"
+	patternUL=".*UL.*$"
+	mandible=$(ls "$target_dir"/*[Mm]and[ií]bula*.nii.gz)
+	nerves=$(ls "$target_dir"/Nerve*)
+	maxillary=$(ls "$target_dir/"*maxilar.nii.gz)
 fi
+
+# handle case where l/r nerves are in different files
+seg_maths $mandible -mul 0 "$target_dir"/nerves_tmp.nii.gz
+for n in $nerves; do
+	seg_maths "$target_dir"/nerves_tmp.nii.gz -add $n "$target_dir"/nerves_tmp.nii.gz
+done
+nerves="$target_dir/nerves_tmp.nii.gz"
 
 #### THIS IS FOR THE LOWER PART OF THE MOUTH
 # now we search the lower area teeth
-lw_teeth_right=$(find -E $target_dir -type f "${patternLR[@]}" | xargs -n1 | sort -r | xargs)
-lw_teeth_left=$(find -E $target_dir -type f "${patternLL[@]}" | sort)
-lw_teeth=$(echo $lw_teeth_left $lw_teeth_right)
+lw_teeth_right=$(find "$target_dir" -maxdepth 1 -regex $patternLR | xargs -n1 | sort -r | xargs)
+lw_teeth_left=$(find "$target_dir" -maxdepth 1 -regex $patternLL | sort)
+lw_teeth=$(echo "$lw_teeth_left" "$lw_teeth_right")
 
 # If we don't find mandible, STOP
 if [ -z "$mandible" ] && [ -z "$lw_teeth" ]; then
 	echo "Neither Mandible nor lower teeth found."
 else
-	arr=($(echo $mandible $lw_teeth $nerves))
 	# we initialize where we are gonna leave everything
-
-	seg_maths ${arr[0]} -mul 0 "${target_dir}"inf.nii.gz
-	fac=1
-	for i in ${arr[@]}; do
-		seg_maths $i -mul $fac $i
-		$SCRIPT_DIR/intersection.sh -d "${target_dir}" "${target_dir}"inf.nii.gz $i
+	seg_maths "$mandible" -mul 1 "$target_dir"/inf.nii.gz
+	fac=2
+	for teeth in $lw_teeth; do
+		seg_maths "$teeth" -mul $fac "$teeth"
+		$SCRIPT_DIR/intersection.sh -d "${target_dir}" "$target_dir/inf.nii.gz" "$teeth"
 		fac=$((fac + 1))
 	done
+	seg_maths "$nerves" -mul $fac "$nerves"
+	$SCRIPT_DIR/intersection.sh -d "${target_dir}" "$target_dir/inf.nii.gz" "$nerves"
 fi
 
 #### NOW COMES THE UPPER MOUTH
-up_teeth_right=$(find -E $target_dir -type f "${patternUR[@]}" | xargs -n1 | sort -r | xargs)
-up_teeth_left=$(find -E $target_dir -type f "${patternUL[@]}" | sort)
-up_teeth=$(echo $up_teeth_right $up_teeth_left)
+up_teeth_right=$(find "$target_dir" -maxdepth 1 -regex "$patternUR" | xargs -n1 | sort -r | xargs)
+up_teeth_left=$(find "$target_dir" -maxdepth 1 -regex "$patternUL" | sort)
+up_teeth=$(echo "$up_teeth_right" $up_teeth_left)
 
 if [ -z "$maxillary" ] && [ -z "$up_teeth" ]; then
 	echo "Neither maxillary nor upper teeth found"
 else
-	arr=($(echo $maxillary $up_teeth))
-
-	# initialize sup NIFTI
-	seg_maths ${arr[0]} -mul 0 "${target_dir}"sup.nii.gz
+	seg_maths $maxillary -mul 1 "$target_dir"/sup.nii.gz
 
 	# now we iterate through all upper teeth
-	fac=1
-	for i in ${arr[@]}; do
-		seg_maths $i -mul $fac $i
-		$SCRIPT_DIR/intersection.sh -d "${target_dir}" "${target_dir}"sup.nii.gz $i
+	fac=2
+	for teeth in $up_teeth; do
+		seg_maths "$teeth" -mul $fac "$teeth"
+		$SCRIPT_DIR/intersection.sh -d "${target_dir}" "${target_dir}/sup.nii.gz" "$teeth"
 		fac=$((fac + 1))
 	done
 fi
