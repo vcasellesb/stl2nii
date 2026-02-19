8fd2837c21789d7bda347011126cef4be7bc670e
Vicent Caselles <vcasellesb@uoc.edu>
2026-02-19 12:20:17 +0100

    updates in bash scripts

--- bash_utils/intersection.sh
+++ bash_utils/intersection.sh
@@ -17,39 +17,39 @@ done
 shift $(( OPTIND - 1 ))
 
 # positional arguments
-total_accumulated=$1
-new_object=$2
-out=${3:-"$total_accumulated"}
+total_accumulated="$1"
+new_object="$2"
+out="${3:-"$total_accumulated"}"
 
 # flag args
-reverse=${reverse:-"false"}
-default_wd=$(dirname $out)
-working_dir=${working_dir:-$default_wd}
+reverse="${reverse:-"false"}"
+default_wd="$(dirname "$out")"
+working_dir="${working_dir:-$default_wd}"
 
 mkdir "$working_dir"/.tmp_sum/
 
 if $reverse; then
-    echo $total_accumulated has priority over $new_object
+    echo "$total_accumulated" has priority over "$new_object"
 else
-    echo $new_object has priority over $total_accumulated
+    echo "$new_object" has priority over "$total_accumulated"
 fi
 
 # binaritzem la mascara amb tot el que portem sumat
-seg_maths ${total_accumulated} -bin "$working_dir"/.tmp_sum/TA-bin.nii.gz
+seg_maths "${total_accumulated}" -bin "$working_dir/.tmp_sum/TA-bin.nii.gz"
 # binaritzem la nova dent o objecte a afegir
-seg_maths ${new_object} -bin "$working_dir"/.tmp_sum/NO-bin.nii.gz
+seg_maths "${new_object}" -bin "$working_dir/.tmp_sum/NO-bin.nii.gz"
 
 # Compute intersection
-seg_maths "$working_dir"/.tmp_sum/TA-bin.nii.gz -mul "$working_dir"/.tmp_sum/NO-bin.nii.gz "$working_dir"/.tmp_sum/intersection.nii.gz
+seg_maths "$working_dir/.tmp_sum/TA-bin.nii.gz" -mul "$working_dir/.tmp_sum/NO-bin.nii.gz" "$working_dir/.tmp_sum/intersection.nii.gz"
 
 # Flip intersection (everywhere where there is no intersection will be 1, where there is intersection will be 0)
-seg_maths "$working_dir"/.tmp_sum/intersection.nii.gz -sub 1 -abs "$working_dir"/.tmp_sum/intersection-inv.nii.gz
+seg_maths "$working_dir/.tmp_sum/intersection.nii.gz" -sub 1 -abs "$working_dir/.tmp_sum/intersection-inv.nii.gz"
 
 # Jo prefereixo l’opció girada després hi ha l’opció girada on és més important el que ja hi ha i esborrem els voxels conflictius de l’objecte
 if $reverse; then
-    seg_maths ${new_object} -mul "$working_dir"/.tmp_sum/intersection-inv.nii.gz -add ${total_accumulated} ${out}
+    seg_maths "${new_object}" -mul "$working_dir/.tmp_sum/intersection-inv.nii.gz" -add "${total_accumulated}" "${out}"
 else
-    seg_maths ${total_accumulated} -mul "$working_dir"/.tmp_sum/intersection-inv.nii.gz -add ${new_object} ${out}
+    seg_maths "${total_accumulated}" -mul "$working_dir/.tmp_sum/intersection-inv.nii.gz" -add "${new_object}" "${out}"
 fi
 
-rm -r "$working_dir"/.tmp_sum/
+rm -r "$working_dir/.tmp_sum/"
--- bash_utils/sum_all_niftis.sh
+++ bash_utils/sum_all_niftis.sh
@@ -4,94 +4,139 @@ set -e
 
 ## Script to add all parts
 if [[ $# -eq 0 ]]; then
-	echo "No directory specified, using current directory"
-	target_dir="."
+    echo "No directory specified, using current directory"
+    target_dir="."
 else
-	target_dir="$1"
+    target_dir="$1"
 fi
 
-nifti_in_target_files=$(ls "${target_dir}"/*.nii.gz | wc -l)
 
-if [[ $nifti_in_target_files -lt 1 ]]; then
-	echo "No nifti files found in "$target_dir" directory, exitting. Please provide a valid dir"
-	exit 1
-fi
-
-# This part removes the nasty disgusting spaces
-## TODO: do the same but for the dirs (I'm tired of doing this manually)
-if [[ $(ls "$target_dir"/*\ * 2>/dev/null | wc -l) -gt 0 ]]; then
-	for file in "${target_dir}"/*' '*; do
-		mv "$file" $(echo "$file" | tr ' ' '_')
-	done
+nifti_count=$(find "$target_dir" -type f -name "*.nii.gz" | wc -l)
+if [[ $nifti_count -lt 1 ]]; then
+    echo "No nifti files found in $target_dir directory, exiting. Please provide a valid dir"
+    exit 1
 fi
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-if [[ $(ls "$target_dir"/*[0-9][0-9]*.nii.gz 2>/dev/null | wc -l) -gt 0 ]]; then
-	patternLR="$target_dir/.*4[1-9].*\.nii.gz$"
-	patternLL="$target_dir/.*3[1-9].*\.nii.gz$"
-	patternUR="$target_dir/.*1[1-9].*\.nii.gz$"
-	patternUL="$target_dir/.*2[1-9].*\.nii.gz$"
-	mandible=$(find "$target_dir" -maxdepth 1 -iregex "$target_dir/mand[ií]u\{0,1\}bl[ea].*\.nii.gz" 2>/dev/null)
-	nerves=$(find "$target_dir" -maxdepth 1 -iregex "$target_dir/nervi\{0,1\}[oe]s.*\.nii.gz" 2>/dev/null)
-	# -E so that we get modern regex. This allows us not to escape {...} and |
-	maxillary=$(find -E "$target_dir" -maxdepth 1 -iregex "$target_dir/.*(maxill{0,1}ary{0,1}|skull).*\.nii.gz" 2>/dev/null)
+
+# if files are numbered, we are dealing with NEMOTEC; otherwise with Bluesky
+numbered_files=$(find "$target_dir" -maxdepth 1 -name "*[0-9][0-9]*.nii.gz" -print -quit)
+
+if [[ -n "$numbered_files" ]]; then
+    # We store the search arguments in arrays to pass them cleanly to `find` later
+    find_args_mandible=("-iregex" ".*/mand[ií]u?bl[ea].*\.nii.gz")
+    find_args_nerves=("-iregex" ".*/nervi?[oe]s.*\.nii.gz")
+    find_args_maxillary=("-iregex" ".*/(maxill?ary?|skull).*\.nii.gz")
+    
+    patternLR=".*/.*4[1-9].*\.nii.gz$"
+    patternLL=".*/.*3[1-9].*\.nii.gz$"
+    patternUR=".*/.*1[1-9].*\.nii.gz$"
+    patternUL=".*/.*2[1-9].*\.nii.gz$"
 else
-	patternLR=".*LR.*$"
-	patternLL=".*LL.*$"
-	patternUR=".*UR.*$"
-	patternUL=".*UL.*$"
-	mandible=$(ls "$target_dir"/*[Mm]and[ií]bula*.nii.gz)
-	nerves=$(ls "$target_dir"/Nerve*)
-	maxillary=$(ls "$target_dir/"*maxilar.nii.gz)
+    find_args_mandible=("-iregex" ".*/[Mm]and[ií]bula.*\.nii.gz")
+    find_args_nerves=("-name" "Nerve*")
+    find_args_maxillary=("-name" "*maxilar.nii.gz")
+
+    patternLR=".*/.*LR.*\.nii.gz$"
+    patternLL=".*/.*LL.*\.nii.gz$"
+    patternUR=".*/.*UR.*\.nii.gz$"
+    patternUL=".*/.*UL.*\.nii.gz$"
 fi
 
-# handle case where l/r nerves are in different files
-seg_maths $mandible -mul 0 "$target_dir"/nerves_tmp.nii.gz
-for n in $nerves; do
-	seg_maths "$target_dir"/nerves_tmp.nii.gz -add $n "$target_dir"/nerves_tmp.nii.gz
-done
-nerves="$target_dir/nerves_tmp.nii.gz"
+# 'IFS= ' sets the Internal Field Separator to the null character, so that whitespaces are not trimmed
+# during variable assignment (by default, IFS is white space, tab and newline). 
+# "read d ''" makes the command not split strings by white space (default), but rather by what find
+# sends it with '-print0', which is the null character.
+IFS= read -r -d '' mandible < <(find -E "$target_dir" -maxdepth 1 "${find_args_mandible[@]}" -print0)
+IFS= read -r -d '' maxillary < <(find -E "$target_dir" -maxdepth 1 "${find_args_maxillary[@]}" -print0)
+
+# Safely extract multiple files into arrays using null-separators (-d '') and zero-byte sorting (-z)
+nerves_array=()
+while IFS= read -r -d "" nerve; do
+	nerves_array+=("$nerve")
+done < <(find "$target_dir" -maxdepth 1 "${find_args_nerves[@]}" -print0 | sort -z)
+
+# Helper function to find and sort teeth, outputting a null-terminated stream
+find_teeth() {
+    local pattern="$1"
+    local sort_flag="$2"
+    if [[ "$sort_flag" == "rev" ]]; then
+        find -E "$target_dir" -maxdepth 1 -regex "$pattern" -print0 | sort -z
+    else
+        find -E "$target_dir" -maxdepth 1 -regex "$pattern" -print0 | sort -z -r
+    fi
+}
+
+# Populate teeth arrays
+lw_teeth_right=()
+while IFS= read -r -d "" teeth; do
+	lw_teeth_right+=("$teeth")
+done < <(find_teeth "$patternLR" "rev")
+
+lw_teeth_left=()
+while IFS= read -r -d "" teeth; do
+	lw_teeth_left+=("$teeth")
+done < <(find_teeth "$patternLL" "")
+
+lw_teeth=("${lw_teeth_left[@]}" "${lw_teeth_right[@]}")
+
+up_teeth_right=()
+while IFS= read -r -d "" teeth; do
+	up_teeth_right+=("$teeth")
+done < <(find_teeth "$patternUR" "rev")
+
+up_teeth_left=()
+while IFS= read -r -d "" teeth; do
+	up_teeth_left+=("$teeth")
+done < <(find_teeth "$patternUL" "")
+
+up_teeth=("${up_teeth_right[@]}" "${up_teeth_left[@]}")
+
+# Handle case where l/r nerves are in different files
+nerves_combined="$target_dir/nerves_tmp.nii.gz"
+if [[ ${#nerves_array[@]} -gt 0 ]]; then
+    seg_maths "$mandible" -mul 0 "$nerves_combined"
+    for nerve in "${nerves_array[@]}"; do
+        seg_maths "$nerves_combined" -add "$nerve" "$nerves_combined"
+    done
+else
+    nerves_combined=""
+fi
 
 #### THIS IS FOR THE LOWER PART OF THE MOUTH
-# now we search the lower area teeth
-lw_teeth_right=$(find "$target_dir" -maxdepth 1 -regex $patternLR | xargs -n1 | sort -r | xargs)
-lw_teeth_left=$(find "$target_dir" -maxdepth 1 -regex $patternLL | sort)
-lw_teeth=$(echo "$lw_teeth_left" "$lw_teeth_right")
-
-# If we don't find mandible, STOP
-if [ -z "$mandible" ] && [ -z "$lw_teeth" ]; then
-	echo "Neither Mandible nor lower teeth found."
+if [[ -z "$mandible" ]] && [[ ${#lw_teeth[@]} -eq 0 ]]; then
+    echo "Neither Mandible nor lower teeth found."
 else
-	# we initialize where we are gonna leave everything
-	seg_maths "$mandible" -mul 1 "$target_dir"/inf.nii.gz
-	fac=2
-	for teeth in $lw_teeth; do
-		seg_maths "$teeth" -mul $fac "$teeth"
-		$SCRIPT_DIR/intersection.sh -d "${target_dir}" "$target_dir/inf.nii.gz" "$teeth"
-		fac=$((fac + 1))
-	done
-	seg_maths "$nerves" -mul $fac "$nerves"
-	$SCRIPT_DIR/intersection.sh -d "${target_dir}" "$target_dir/inf.nii.gz" "$nerves"
+    # we initialize where we are gonna leave everything
+    seg_maths "$mandible" -mul 1 "$target_dir/inf.nii.gz"
+    fac=2
+    for teeth in "${lw_teeth[@]}"; do
+        seg_maths "$teeth" -mul $fac "$teeth"
+        "$SCRIPT_DIR/intersection.sh" -d "${target_dir}" "$target_dir/inf.nii.gz" "$teeth"
+        fac=$((fac + 1))
+    done
+
+    # Process combined nerves if they exist
+    if [[ -n "$nerves_combined" && -f "$nerves_combined" ]]; then
+        seg_maths "$nerves_combined" -mul $fac "$nerves_combined"
+        "$SCRIPT_DIR/intersection.sh" -d "${target_dir}" "$target_dir/inf.nii.gz" "$nerves_combined"
+    fi
 fi
 
 #### NOW COMES THE UPPER MOUTH
-up_teeth_right=$(find "$target_dir" -maxdepth 1 -regex "$patternUR" | xargs -n1 | sort -r | xargs)
-up_teeth_left=$(find "$target_dir" -maxdepth 1 -regex "$patternUL" | sort)
-up_teeth=$(echo "$up_teeth_right" $up_teeth_left)
-
-if [ -z "$maxillary" ] && [ -z "$up_teeth" ]; then
-	echo "Neither maxillary nor upper teeth found"
+if [[ -z "$maxillary" ]] && [[ ${#up_teeth[@]} -eq 0 ]]; then
+    echo "Neither maxillary nor upper teeth found"
 else
-	seg_maths $maxillary -mul 1 "$target_dir"/sup.nii.gz
+    seg_maths "$maxillary" -mul 1 "$target_dir/sup.nii.gz"
 
-	# now we iterate through all upper teeth
-	fac=2
-	for teeth in $up_teeth; do
-		seg_maths "$teeth" -mul $fac "$teeth"
-		$SCRIPT_DIR/intersection.sh -d "${target_dir}" "${target_dir}/sup.nii.gz" "$teeth"
-		fac=$((fac + 1))
-	done
+    # now we iterate through all upper teeth
+    fac=2
+    for teeth in "${up_teeth[@]}"; do
+        seg_maths "$teeth" -mul $fac "$teeth"
+        "$SCRIPT_DIR/intersection.sh" -d "${target_dir}" "${target_dir}/sup.nii.gz" "$teeth"
+        fac=$((fac + 1))
+    done
 fi
 
 echo "Done"
